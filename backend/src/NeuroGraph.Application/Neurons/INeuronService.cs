d8cc59948c7797cd26febef0174aa598f70a87ac
Joao V <augustoviegascs@gmail.com>
2026-06-02 20:28:22 -0300

    reajusted

--- backend/src/NeuroGraph.Application/Neurons/INeuronService.cs
+++
@@ -1,8 +0,0 @@
-namespace NeuroGraph.Application.Neurons;
-
-public interface INeuronService
-{
-    Task<IReadOnlyList<NeuronDto>> GetAllAsync(CancellationToken ct = default);
-    Task<NeuronDto?> GetByIdAsync(Guid id, CancellationToken ct = default);
-    Task<NeuronDto> CreateAsync(CreateNeuronRequest request, CancellationToken ct = default);
-}
