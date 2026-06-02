26ab8a27d19891af760591b08cc0b0148ce28e00
Joao V <augustoviegascs@gmail.com>
2026-06-02 20:28:22 -0300

    reajusted

--- backend/src/NeuroGraph.Domain/Enums/SignalType.cs
+++
@@ -1,9 +0,0 @@
-namespace NeuroGraph.Domain.Enums;
-
-/// <summary>Natureza de um sinal biológico que trafega entre neurônios.</summary>
-public enum SignalType
-{
-    Excitatory = 0,
-    Inhibitory = 1,
-    Modulatory = 2
-}
