6df629e73635894d592725c01a9b8590491c4be8
Joao V <augustoviegascs@gmail.com>
2026-06-04 15:33:42 -0300

    added new adjustments on entities

--- Core/NeuroGraph.Main/Entities/BiologicalSignal.cs
+++ Core/NeuroGraph.Main/Entities/BiologicalSignal.cs
@@ -4,8 +4,8 @@ namespace Api.Entities
     {
        public Guid Id { get; set; }
         public Guid NeuronId { get; set; }          // FK
-        public Neuron Neuron { get; set; } = null!;
-        public double Intensity { get; set; }
-        public DateTime FiredAt { get; set; } = DateTime.UtcNow;
+        //public Neuron Neuron { get; set; } = null!;
+        public float Intensity { get; set; }
+        public DateTimeOffset FiredAt { get; set; } = DateTimeOffset.UtcNow;
     }
 }
