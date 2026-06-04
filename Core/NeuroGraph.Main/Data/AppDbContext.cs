90e809b0b92adbdc75e87cfe5598f07213898215
keven <mkminato123@gmail.com>
2026-06-04 13:44:10 -0300

    add

--- Core/NeuroGraph.Main/Data/AppDbContext.cs
+++ Core/NeuroGraph.Main/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Api.Entities;
 
 namespace NeuroGraph.Main.Data;
 
@@ -7,4 +8,22 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     // Os DbSet<> (tabelas) entram aqui na PRÓXIMA issue.
+    public DbSet<Neuron> Neurons => Set<Neuron>();
+    public DbSet<BiologicalEvent> BiologicalEvents => Set<BiologicalEvent>();
+    public DbSet<BiologicalSignal> BiologicalSignals => Set<BiologicalSignal>();
+    public DbSet<NeuralConnection> NeuralConnections => Set<NeuralConnection>();
+    public DbSet<NeuronLog> NeuronLogs => Set<NeuronLog>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<NeuralConnection>()
+            .HasOne<Neuron>().WithMany()
+            .HasForeignKey(c => c.SourceNeuronId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<NeuralConnection>()
+            .HasOne<Neuron>().WithMany()
+            .HasForeignKey(c => c.TargetNeuronId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
---
+++ Core/NeuroGraph.Main/Entities/NeuronLog.cs
@@ -0,0 +1,7 @@
+namespace Api.Entities
+{
+    public class NeuronLog
+    {
+       
+    }
+}
